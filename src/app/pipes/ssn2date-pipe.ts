e1bf1c75f497a863882ec7cbef9fad49c9e76b51
mali <mohamedalim559@gmail.com>
2025-10-19 13:15:14 +0300

    Refactor Ssn2datePipe to return a formatted date string; update Order component to reflect changes in date display

--- src/app/pipes/ssn2date-pipe.ts
+++ src/app/pipes/ssn2date-pipe.ts
@@ -8,14 +8,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class Ssn2datePipe implements PipeTransform {
 
   // 30301141202434
-  transform(value :number):Date{
+  transform(value :number):string{
     let ssnString = value.toString();
     let year = parseInt(ssnString.substring(1,3));
     let month = parseInt(ssnString.substring(3,5)); // Months are zero-based in JS Date
     let day = parseInt(ssnString.substring(5,7));
-    
-    return new Date(2000 + year, month - 1, day); // Adjust year as needed
-
+   
+    return (year+2000).toString() + '-' + month.toString() + '-' + day.toString();
   }
 
 }
