2870e24c6a1d3a3a071294c3f5051e912c9ecdbb
mali <mohamedalim559@gmail.com>
2025-10-19 08:39:09 +0300

    Update ProdCard directive: add ExternalBgColor input and enhance hover effects; adjust Products component layout

--- src/app/directives/prod-card.ts
+++ src/app/directives/prod-card.ts
@@ -1,13 +1,24 @@
-import { Directive  , ElementRef} from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appProdCard]'
 })
 export class ProdCard {
+  @Input() ExternalBgColor: string = 'black';
 
-  constructor(ele:ElementRef) {
-    ele.nativeElement.style.borderRadius = '15px';
-    ele.nativeElement.style.boxShadow = '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)';
+  constructor(private ele: ElementRef) {
+    this.ele = ele;
+    this.ele.nativeElement.style.borderRadius = '15px';
+    this.ele.nativeElement.style.boxShadow = '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)';
   }
 
+  @HostListener('mouseover') over() {
+    this.ele.nativeElement.style.boxShadow = '0 8px 16px 0 rgba(0, 0, 0, 0.3), 0 12px 40px 0 rgba(0, 0, 0, 0.25)';
+    this.ele.nativeElement.style.backgroundColor = this.ExternalBgColor;
+  }
+  
+  @HostListener('mouseout') out() {
+    this.ele.nativeElement.style.boxShadow = '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)';
+    this.ele.nativeElement.style.backgroundColor = 'white';
+  }
 }
